64640c0eee8a35a9dfbdd5631b8546fdcf54a454
ShangJin Li <shli001@uw.edu>
2020-03-29 20:34:43 -0700

    user

--- polluser/User.go
+++ polluser/User.go
@@ -4,11 +4,11 @@ import "time"
 
 //An user object with info in it
 type User struct {
-	UID  string `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name string
-	pass []byte
-	salt []byte
-
+	UID        string `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name       string
+	pass       []byte
+	salt       []byte
+	pollGroup  map[string]uint32
 	CreateTime time.Time
 }
 
