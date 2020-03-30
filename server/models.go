e6605951c1f17171845169748e1b1de29c672df7
Shangrong Li <lil2op@Shangrongteki-MacBook-Pro.local>
2020-03-29 22:14:10 -0700

    22.14

--- server/models.go
+++ server/models.go
@@ -1,5 +1,6 @@
 package server
 
+// Params
 const uArrayNumElements = 2
 
 const uParamsTopic = 1
@@ -17,3 +18,10 @@ const uParamsPassword = 1
 const uParamsPollID = 0
 
 const uParamsVotes = 1
+
+// CONSTANTS
+const GET_POLL_NUM = 10
+
+const VOTE_MUL_PARAM_NUM = 2
+
+const START_PG_PARAM_NUM = 1
